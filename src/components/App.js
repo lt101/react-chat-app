da300bc6a2eec0eaef67caacb2623c9f04adb2df
liemtruong1 <liem.truong@polymtl.ca>
2022-03-24 09:27:05 -0400

    integrate context to contacts

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React from 'react'
 import useSessionStorage from '../hooks/useSessionStorage'
 import Login from './Login'
 import Dashboard from './Dashboard'
+import { ContactsProvider } from '../contexts/ContactsProvider'
 
 const App = () => {
   const [id, setId] = useSessionStorage('id')
 
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  )
+
   return (
-      id? <Dashboard id={id} /> : <Login onIdSubmit={setId}/>
+      id? dashboard : <Login onIdSubmit={setId}/>
   )
 }
 
--- src/components/NewContactModal.js
+++ src/components/NewContactModal.js
@@ -1,15 +1,17 @@
 import React, { useRef } from 'react'
 import { Modal, Form, Button } from 'react-bootstrap'
+import { useContacts } from '../contexts/ContactsProvider'
 
 const NewContactModal = ({ closeModal }) => {
 
     const idRef = useRef()
     const nameRef = useRef()
+    const { createContact } = useContacts()
 
     function handleSubmit(e) {
         e.preventDefault()
 
-        // createContact(idRef.current.value, nameRef.current.value)
+        createContact(idRef.current.value, nameRef.current.value)
         closeModal()
     }
 
@@ -26,8 +28,8 @@ const NewContactModal = ({ closeModal }) => {
                         <Form.Label>Name</Form.Label>
                         <Form.Control type="text" ref={nameRef} required />
                     </Form.Group>
+                    <Button type="submit">Create</Button>
                 </Form>
-                <Button type="submit">Create</Button>
             </Modal.Body>
         </>
     )
