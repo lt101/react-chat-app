b9c276d05c5652bd1cdb7641cd33771640163916
liemtruong1 <liem.truong@polymtl.ca>
2022-03-24 19:49:02 -0400

    create room component

---
+++ src/components/Rooms.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { ListGroup } from 'react-bootstrap'
+import { RoomContext } from '../contexts/RoomContext'
+
+function Rooms({user}) {
+
+    const COOL_ROOM = "cool room"
+    const COZY_ROOM = "cozy room"
+    const NICE_ROOM = "nice room"
+
+    const {selectedRoom, setselectedRoom} = useContext(RoomContext)
+
+    return (
+        <div style={{ width: '25%' }} className="d-flex flex-column">
+            <div className="border border-right overflow-auto">Welcome {user}! Choose a room: </div>
+                <ListGroup selectedRoom={selectedRoom} onSelect={setselectedRoom}>
+                    <ListGroup.Item eventKey={COOL_ROOM}>Cool room</ListGroup.Item>
+                    <ListGroup.Item eventKey={COZY_ROOM}>Cozy room</ListGroup.Item>
+                    <ListGroup.Item eventKey={NICE_ROOM}>Nice room</ListGroup.Item>
+                </ListGroup>
+        </div>
+    )
+    
+}
+
+export default Rooms
