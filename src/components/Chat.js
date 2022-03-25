0711d0ba6e36f69e18a25e8b4788541bae65b42a
liemtruong1 <liem.truong@polymtl.ca>
2022-03-24 22:31:11 -0400

    chat layout

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,18 +1,34 @@
-import React, { useContext } from 'react'
-import { Tab } from 'react-bootstrap'
+import React, { useState, useContext } from 'react'
+import { Form, InputGroup, Button } from 'react-bootstrap'
 import { RoomContext } from '../contexts/RoomContext'
 
 function Chat() {
 
     const {selectedRoom} = useContext(RoomContext)
+    const [text, setText] = useState('')
+
+    function handleSubmit(e) {
+
+    }
 
     return (
-        <div style={{ width: '75%' }} className="d-flex flex-column">
-            <Tab.Container>
-                <div>Showing chat from {selectedRoom}</div>
-                <Tab.Content className="border border-right overflow-auto flex-grow-1">
-                </Tab.Content>
-            </Tab.Container>
+        <div style={{ width: '75%' }} className="d-flex flex-column flex-grow-1"> 
+            <div>Showing chat from {selectedRoom}</div>
+            <div className="border border-left overflow-auto flex-grow-1"></div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="m-1">
+                    <InputGroup>
+                        <Form.Control
+                            as="textarea"
+                            required
+                            value={text}
+                            onChange={e => setText(e.target.value)}
+                            style={{ height: '75px', resize: 'none' }}
+                        />     
+                        <Button type="submit">Send</Button>
+                    </InputGroup>
+                </Form.Group>
+            </Form>
         </div>
     )
     
